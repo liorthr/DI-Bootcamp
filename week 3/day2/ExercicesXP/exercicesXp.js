ed9c02a1f28b4ae640b99f2e34c24cd9e741ae7d
liorthr <liortahar116@gmail.com>
2023-12-11 11:26:54 +0200

    exercices-xp-end

---
+++ week 3/day2/ExercicesXP/exercicesXp.js
@@ -0,0 +1,178 @@
+
+// EX 1
+/**let favoriteFood = "Couscous boulette"
+let favoriteMeal = "breakfast"
+console.log("I eat " +favoriteFood + " at every " + favoriteMeal)*/
+
+
+//EX 2 Part 1
+/*const myWatchedSeries = ["Black mirror", "money heist", "the big bang theory"];
+let myWatchedSeriesLength = myWatchedSeries.length
+console.log(myWatchedSeriesLength)
+
+let myWatchedSeriesSentence = myWatchedSeries[0] + ", " + myWatchedSeries[1] + " and " + myWatchedSeries[2]
+console.log(myWatchedSeriesSentence)
+
+//EX 2 Part 2
+myWatchedSeries[2] = "friends"
+myWatchedSeries.push("la casa del papel")
+myWatchedSeries.unshift("Fauda")
+myWatchedSeries.splice(1,1)
+console.log(myWatchedSeries[2][4])
+console.log(myWatchedSeries)*/
+
+//EX 3
+
+/*let temperatureCelcius = 20
+let temperatureFarenheit = (temperatureCelcius * 9/5) + 32
+console.log(temperatureCelcius + "C° is " + temperatureFarenheit + " F")*/
+
+//EX 4
+
+   /* let c;
+    let a = 34;
+    let b = 21;
+
+    console.log(a+b) //first expression
+    // Prediction: 55
+    // Actual: 55
+
+    a = 2;
+
+    console.log(a+b) //second expression
+    // Prediction: 23
+    // Actual: 23
+
+    //3.What is the value of c?
+    //c is undefined
+
+    //console.log(3 + 4 + '5');
+    //Prediction : 75
+    //Actual: 75 */
+
+
+//EX 5
+
+
+    /*console.log(typeof(15))  
+    // Prediction: number
+    // Actual: number 
+
+    console.log(typeof(5.5))
+    // Prediction: number
+    // Actual: number
+
+    console.log(typeof(NaN))
+    // Prediction: boolean
+    // Actual: number
+
+    console.log(typeof("hello"))
+    // Prediction string
+    // Actual: string 
+
+    console.log(typeof(true))
+    // Prediction:boolean
+    // Actual: boolean 
+
+    console.log(typeof(1 != 2))
+    // Prediction: number
+    // Actual: boolean 
+
+    console.log("hamburger" + "s")
+    // Prediction: hamburgers
+    // Actual: hamburgers 
+
+    console.log("hamburgers" - "s")
+    // Prediction: hamburger
+    // Actual: NaN
+
+    console.log("1" + "3")
+    // Prediction: 13
+    // Actual: 13 
+
+    console.log("1" - "3")
+    // Prediction: NaN
+    // Actual: -2 
+
+    console.log("johnny" + 5)
+    // Prediction: NaN
+    // Actual: johnny5
+
+    console.log("johnny" - 5)   
+    // Prediction: NaN
+    // Actual: NaN 
+
+    console.log(99 * "hello")
+    // Prediction: hello 99 times
+    // Actual: NaN 
+
+    console.log(1 != 1)
+    // Prediction: false
+    // Actual: false
+
+    console.log(1 == "1")
+    // Prediction: false
+    // Actual: true // I dont understand why a number can, be equal a string 
+
+
+    console.log(1 === "1")
+    // Prediction: false
+    // Actual: false */
+
+//EX 6
+
+   /* console.log(5 + "34")
+    // Prediction: 534
+    // Actual: 534
+
+    console.log(5 - "4")
+    // Prediction: Nan
+    // Actual: 1
+
+    console.log(10 % 5)
+    // Prediction: 0
+    // Actual:0 
+
+    console.log(5 % 10)
+    // Prediction:0
+    // Actual: 5 // in think its 0.5 and the reminder it's 0 
+
+    console.log("Java" + "Script")
+    // Prediction: JavaScript
+    // Actual: JavaScript 
+
+    console.log(" " + " ")
+    // Prediction: 
+    // Actual: 
+
+    console.log(" " + 0)
+    // Prediction: 0 // With a space before the zero
+    // Actual: 0 
+
+    console.log(true + true)
+    // Prediction: true
+    // Actual: 2
+
+    console.log(true + false)
+    // Prediction: 0
+    // Actual: 1 
+
+    console.log(false + true)
+    // Prediction: 1
+    // Actual:1 
+
+    console.log(false - true)
+    // Prediction: -1
+    // Actual: -1 
+
+    console.log(!true)
+    // Prediction: true
+    // Actual: false 
+
+    console.log(3 - 4)
+    // Prediction: -1
+    // Actual: -1 
+
+    console.log("Bob" - "bill")
+    // Prediction: NaN
+    // Actual: NaN */
