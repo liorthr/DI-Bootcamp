f6964b395b5816d73da6fc4973a5a570b5e3e4b6
liorthr <liortahar116@gmail.com>
2023-12-12 13:31:22 +0200

    intercative-form-test-1

---
+++ week 3/day3/interactive-form-test/script.js
@@ -0,0 +1,26 @@
+const openFormBtn = document.getElementById('openFormBtn');
+const formModal = document.getElementById('formModal');
+const step1 = document.getElementById('step1');
+const step2 = document.getElementById('step2');
+const step3 = document.getElementById("step3")
+const nextStep1Btn = document.getElementById('nextStep1');
+const nextStep2Btn = document.getElementById('nextStep2');
+const nextStep3Btn = document.getElementById('nextStep3');
+
+
+
+openFormBtn.addEventListener('click', function() {
+  formModal.style.display = 'block';
+});
+
+nextStep1Btn.addEventListener('click', function() {
+  step1.style.display = 'none';
+  step2.style.display = 'block';
+});
+nextStep2Btn.addEventListener('click', function() {
+    step2.style.display = 'none';
+    step3.style.display = 'block';
+  });
+nextStep3Btn.addEventListener('click', function() {
+  formModal.style.display = 'none';
+});
