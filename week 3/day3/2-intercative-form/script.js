97882d593bae5f2946babe0891c9558c4667a47c
liorthr <liortahar116@gmail.com>
2023-12-13 13:03:44 +0200

    dontinue progress

--- week 3/day3/2-intercative-form/script.js
+++ week 3/day3/2-intercative-form/script.js
@@ -3,9 +3,11 @@ const formModal = document.getElementById('formModal');
 const step1 = document.getElementById('step1');
 const step2 = document.getElementById('step2');
 const step3 = document.getElementById("step3")
+const step4 = document.getElementById("step4")
 const nextStep1Btn = document.getElementById('nextStep1');
 const nextStep2Btn = document.getElementById('nextStep2');
 const nextStep3Btn = document.getElementById('nextStep3');
+const nextStep4Btn = document.getElementById('nextStep4')
 
 
 
@@ -21,6 +23,10 @@ nextStep2Btn.addEventListener('click', function() {
     step2.style.display = 'none';
     step3.style.display = 'block';
   });
-nextStep3Btn.addEventListener('click', function() {
+  nextStep3Btn.addEventListener('click', function() {
+    step3.style.display = 'none';
+    step4.style.display = 'block';
+  });
+nextStep4Btn.addEventListener('click', function() {
   formModal.style.display = 'none';
 });
