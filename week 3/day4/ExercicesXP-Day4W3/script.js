110d67d5c5302100e01373fc28d78b8961dd4fb4
liorthr <liortahar116@gmail.com>
2023-12-13 11:02:19 +0200

    exercicesxp done

---
+++ week 3/day4/ExercicesXP-Day4W3/script.js
@@ -0,0 +1,46 @@
+//EX 1
+
+/*let x = 1
+let y = 3
+if (x > y){
+    console.log("X is the biggest number")
+}else{
+    console.log("Y is the biggest number")
+}*/
+
+//EX 2
+
+/*let newDog = "Chihuahua"
+let newDogLetters = newDog.length
+console.log(newDogLetters)
+console.log(newDog.toUpperCase())
+console.log(newDog.toLowerCase())
+ if (newDog === "Chihuahua"){
+    console.log("I love Chihuahuas, it’s my favorite dog breed")
+ }else{
+    console.log("I dont care, I prefer cats")
+ }*/
+
+ //EX 3
+
+ /*let numberUser = prompt("Write a number: ")
+ if (numberUser % 2 == 0 ){
+    console.log(numberUser + " is an even number")
+ } else {
+    console.log(numberUser + " is an odd number")
+ }*/
+
+ //EX 4
+
+ /*const users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+ if (users.length <= 0 ){
+    console.log("no one is online")
+ } else if (users.length == 1){
+    console.log(users[0] + " is online ")
+ } else if (users.length == 2){
+    console.log(users[0] + " and " + users[1] + " are online")
+ } else{
+    console.log(users[0] + ", " + users[1] + " and " + (users.length - 2)+ " more are online")
+ }*/
+ // In the last condition we know already know the name of 2 users then
+ //we need to remove them 
