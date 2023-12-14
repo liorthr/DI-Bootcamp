71e0868c91f2efcaeb54ab2b9fa122b9f0b45031
liorthr <liortahar116@gmail.com>
2023-12-14 12:03:02 +0200

    interactive form

--- week 3/day3/2-intercative-form/script.js
+++ week 3/day3/2-intercative-form/script.js
@@ -4,10 +4,12 @@ const step1 = document.getElementById('step1');
 const step2 = document.getElementById('step2');
 const step3 = document.getElementById("step3")
 const step4 = document.getElementById("step4")
+const step5 = document.getElementById("step5")
 const nextStep1Btn = document.getElementById('nextStep1');
 const nextStep2Btn = document.getElementById('nextStep2');
 const nextStep3Btn = document.getElementById('nextStep3');
 const nextStep4Btn = document.getElementById('nextStep4')
+const nextStep5Btn = document.getElementById('nextStep5')
 
 
 
@@ -27,6 +29,21 @@ nextStep2Btn.addEventListener('click', function() {
     step3.style.display = 'none';
     step4.style.display = 'block';
   });
-nextStep4Btn.addEventListener('click', function() {
+  nextStep4Btn.addEventListener('click', function() {
+    step4.style.display = 'none';
+    step5.style.display = 'block';
+  });
+nextStep5Btn.addEventListener('click', function() {
   formModal.style.display = 'none';
 });
+
+
+/*telephone champ*/
+/*function validatePhoneNumber(input) {
+  const phoneNumber = input.value.replace(/\D/g, ''); // Supprime tout sauf les chiffres
+  if (phoneNumber.length !== 8) {
+      input.setCustomValidity('Le numéro de téléphone doit comporter exactement 8 chiffres.');
+  } else {
+      input.setCustomValidity('');
+  }
+}*/
