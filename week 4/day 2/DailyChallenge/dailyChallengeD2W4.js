40c036de67f037e9b8b8876e71c8646666c86e2a
liorthr <liortahar116@gmail.com>
2023-12-18 16:20:15 +0200

    daily challenge advance

--- week 4/day 2/DailyChallenge/dailyChallengeD2W4.js
+++ week 4/day 2/DailyChallenge/dailyChallengeD2W4.js
@@ -1,3 +1,8 @@
 let userWord = prompt("Write a list of word with a comma between each word")
 let arrOfUserWord = userWord.split(',').map(word => word.trim())
 console.log(arrOfUserWord)
+
+for(let x of arrOfUserWord){
+    arrOfUserWord[x] = " * " + arrOfUserWord + " * "
+}
+console.log(arrOfUserWord)
