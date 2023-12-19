399c0d269678274649d8de9bd73aa88298d746df
liorthr <liortahar116@gmail.com>
2023-12-19 11:05:14 +0200

    daily challenge done

--- week 4/day 2/DailyChallenge/dailyChallengeD2W4.js
+++ week 4/day 2/DailyChallenge/dailyChallengeD2W4.js
@@ -1,5 +1,5 @@
 
-
+/*
 let userWord = prompt("Write a list of words with a comma between each word");
 let arrOfUserWord = userWord.split(',').map(word => word.trim());
 //console.log(arrOfUserWord);
@@ -16,5 +16,29 @@ arrOfUserWord.forEach(word => {
 });
 //console.log(arrOfUserWord);
 console.log(star)
-
+*/
 //At the top its what i try to fix
+function printRectangularFrame(words) {
+    let maxLength = 0;
+    for (let word of words) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+        }
+    }
+
+    // We use the lenghth of the biggest word 
+    const delimiterStarTopBottom = "*".repeat(maxLength + 4)
+    console.log(delimiterStarTopBottom);
+
+    // ChatGPT
+    for (let word of words) {
+        console.log(`* ${word}${' '.repeat(maxLength - word.length)} *`);
+    }
+
+    console.log(delimiterStarTopBottom);
+}
+
+let userInput = prompt("Enter several words separated by commas:");
+let wordsArray = userInput.split(',').map(word => word.trim());
+
+printRectangularFrame(wordsArray);
