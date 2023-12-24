0e28b201cd18151b49646b5a56dce5dfc39a47da
liorthr <liortahar116@gmail.com>
2023-12-24 12:42:17 +0200

    daily challenge done

--- week 4/day 5/daily challenge/dailyChallengeW4D5.js
+++ week 4/day 5/daily challenge/dailyChallengeW4D5.js
@@ -1,14 +1,29 @@
-let userNumber = prompt("Hey bro write a number:")
-let numberOfBeer = userNumber
 
-for(let i= 1; i<50; i++){
-    console.log(numberOfBeer)
-    //console.log(i)
-    numberOfBeer = numberOfBeer - i
-    console.log(numberOfBeer)
-    if(numberOfBeer<=0){
-        break
+function mySong(){
+    let userNumber = prompt("Hey bro write a number:")
+    let numberOfBeer = parseInt(userNumber)
+    for(let i= 1; i<numberOfBeer; i++){
+
+        if(i === 1){
+            console.log(numberOfBeer +" bottles of beer on the wall")
+            console.log(numberOfBeer +" bottles of beer")
+            console.log('Take ' + i + " down, pass it around")
+            numberOfBeer = numberOfBeer - i
+            console.log(numberOfBeer +" bottles of beer on the wall")
+            console.log("  ")
+        }else {
+            console.log(numberOfBeer +" bottles of beer on the wall")
+            console.log(numberOfBeer +" bottles of beer")
+            console.log('Take ' + i + " down, pass them around")
+            numberOfBeer = numberOfBeer - i
+            console.log(numberOfBeer +" bottles of beer on the wall")
+            console.log("  ")
+        }
+        if (i === 0 || i<0) {
+            console.log("No more bottles of beer on the wall");
+            break
+        }
     }
-}
 
-  
+}
+mySong()
