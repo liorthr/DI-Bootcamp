db26157f8461ec9d6ccfb3fbeae95ff53abd7ffc
liorthr <liortahar116@gmail.com>
2023-12-27 13:49:00 +0200

    steinterval and settimeout

--- week 5/day 4/lesson check/script.js
+++ week 5/day 4/lesson check/script.js
@@ -10,14 +10,43 @@
             //     //     console.log("Cleared")
             //     // }
             // }, 1000)
-let counter = 0
-    const timerID = setTimeout(function(){
-    window.location.href = "https://google.com"
-}, 5000)
+// let counter = 0
+//     const timerID = setTimeout(function(){
+//     window.location.href = "https://google.com"
+// }, 5000)
 
-const btnJs = document.getElementById("btn")
-btnJs.addEventListener("click", function(){
-    clearInterval(timerID)
-    console.log("finsh")
-})
+// const btnJs = document.getElementById("btn")
+// btnJs.addEventListener("click", function(){
+//     clearInterval(timerID)
+//     console.log("finsh")
+// })
 
+
+// const btnStart = document.getElementById("btn-start")
+// const btnClear = document.getElementById("btn-clear")
+// btnStart.addEventListener("click", displaycount)
+// btnClear.addEventListener("click", stopCount)
+// let count = 10
+// let id
+
+// function displaycount(){
+//     console.log(count)
+//     count--
+//     if(count >= 0){
+//         id = setTimeout(displaycount, 1000);
+//     }
+// }  
+// function stopCount(){
+//     clearTimeout(id)
+// }
+
+function showmessgae(){
+    let bodyJS = document.querySelector("body")
+    let h1JS = document.createElement("h1")
+    bodyJS.append(h1JS)
+    let textMessgae = "New text"
+    h1JS.textContent = textMessgae
+}
+const id = setInterval(showmessgae, 2000)
+
+clearInterval(id)
