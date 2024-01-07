dabdf837d3b2cab5fdc1f13828986eefc1a10b25
liorthr <liortahar116@gmail.com>
2024-01-07 15:31:12 +0200

    exercices xp done

---
+++ week-6/day-5/exercices-xp-.d5-w6sql.sql
@@ -0,0 +1,5 @@
+/*SELECT * FROM items WHERE price > 80;
+SELECT * FROM items WHERE price <= 300;
+SELECT * FROM customers WHERE last_name = 'Smith'
+SELECT * FROM customers WHERE last_name = 'Jones'
+SELECT * FROM customers WHERE first_name != 'Scott'*/
