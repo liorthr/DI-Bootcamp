1eddc3e19f8010493059258c428d7d1f727b1624
liorthr <liortahar116@gmail.com>
2024-01-14 07:54:52 +0200

    daily challenge done

---
+++ week-7/day 6/daily challenge/dailyChallengeD6W7.js
@@ -0,0 +1,54 @@
+const gameInfo = [
+    {
+      username: "john",
+      team: "red",
+      score: 5,
+      items: ["ball", "book", "pen"]
+    },
+    {
+      username: "becky",
+      team: "blue",
+      score: 10,
+      items: ["tape", "backpack", "pen"]
+    },
+    {
+      username: "susy",
+      team: "red",
+      score: 55,
+      items: ["ball", "eraser", "pen"]
+    },
+    {
+      username: "tyson",
+      team: "green",
+      score: 1,
+      items: ["book", "pen"]
+    },
+   ];
+   
+
+//EXERCISE 1
+
+//    const usernames = [];
+
+//    gameInfo.forEach(obj => {
+//        const firstElement = obj[Object.keys(obj)[0]];
+//        usernames.push(firstElement);
+//    });
+   
+//    console.log(usernames);
+
+
+///EXERCISE 2
+
+
+    // const winners = [];
+
+    // gameInfo.forEach(obj => {
+    //     const winnersElement = obj[Object.keys(obj)[0]];
+    //     if (obj.score > 5) {
+    //         winners.push(winnersElement);
+    //     }
+    // });
+    
+    // console.log(winners);
+    
