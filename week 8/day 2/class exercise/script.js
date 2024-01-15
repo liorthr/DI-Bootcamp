860fdab1a2f44aa9769a80beb0f02fb3e5cefa05
liorthr <liortahar116@gmail.com>
2024-01-15 15:17:46 +0100

    break

--- week 8/day 2/class exercise/script.js
+++ week 8/day 2/class exercise/script.js
@@ -1,7 +1,37 @@
-let arr1 = [1,2,5];
-//let arr2 = arr1;
-let arr2 = [...arr1]
+// let arr1 = [1,2,5];
+// //let arr2 = arr1;
+// let arr2 = [...arr1]
 
-arr2[0]=29
+// arr2[0]=29
 
-console.log(arr2)
+// console.log(arr2)
+
+// let arr3 = [
+//     {name:"Shanna", age:20},
+//     {name:"Rosa", age:45},
+//     {name:"Mira", age:70},
+//     {name:"Scetbon", age:40},
+// ];
+// let jsonArr = JSON.stringify(arr3);
+// console.log(jsonArr)
+
+// let say = function (name) {
+//     alert("Hello " + name);
+// }         
+// say("Sarah");
+
+// let askName = function(name){
+//     name = prompt('Hey give me your name')
+//     alert(`${name} is a realy bad name`)
+// }
+// askName("ahslkey")
+
+function verify (name) {            // outer function  
+    function isJohn() {             // inner function
+        return name === "John";     // full access to argument        
+    }
+    if (isJohn()) {
+        alert("Yep, this is John");
+    }
+}
+verify("John");
