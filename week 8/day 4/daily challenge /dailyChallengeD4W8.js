c56715de3e9aab69859f499460d52bf1c69b195b
liorthr <liortahar116@gmail.com>
2024-01-17 12:54:43 +0100

    break in daily challenge at ex 1

---
+++ week 8/day 4/daily challenge /dailyChallengeD4W8.js
@@ -0,0 +1,16 @@
+
+const inventory = [
+    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+    { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+    { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+    { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+function getCarHonda(carInventory){
+    for(car in inventory){
+        if(inventory.car_make === "Honda"){
+            return inventory.car_make
+        }
+    }
+};
+getCarHonda(inventory)
