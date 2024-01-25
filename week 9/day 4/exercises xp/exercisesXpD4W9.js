16c6e04e0251005c338f929ff10cb4ac45557cf3
liorthr <liortahar116@gmail.com>
2024-01-25 12:23:47 +0100

    ex xp done

---
+++ week 9/day 4/exercises xp/exercisesXpD4W9.js
@@ -0,0 +1,36 @@
+
+// const marioGame = {
+//     detail : "An amazing game!",
+//     characters : {
+//         mario : {
+//           description:"Small and jumpy. Likes princesses.",
+//           height: 10,
+//           weight: 3,
+//           speed: 12,
+//         },
+//         bowser : {
+//           description: "Big and green, Hates princesses.",
+//           height: 16,
+//           weight: 6,
+//           speed: 4,
+//         },
+//         princessPeach : {
+//           description: "Beautiful princess.",
+//           height: 12,
+//           weight: 2,
+//           speed: 2,
+//         }
+//     },
+//   }
+
+
+//   PART 1
+//   console.log(JSON.stringify(marioGame))
+// All the nested object like ` mario: ` become=>  ` "mario": `
+
+//  PART 2
+// console.log(`JSON conversion reult:
+// ${JSON.stringify(marioGame,null,2)}`)
+
+//PART 3
+//I don't know what do you mean by breakpoint with web inspector
