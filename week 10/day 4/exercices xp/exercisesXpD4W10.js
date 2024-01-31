8c340da51df0891a91f362b3304611c9696940c3
liorthr <liortahar116@gmail.com>
2024-01-31 12:48:26 +0100

    call api test

---
+++ week 10/day 4/exercices xp/exercisesXpD4W10.js
@@ -0,0 +1,29 @@
+//test api 
+// const url = 'https://love-calculator.p.rapidapi.com/getPercentage?sname=Alice&fname=John';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'db968b2843msha045305651672a8p106c75jsnf87bf1c8c53c',
+// 		'X-RapidAPI-Host': 'love-calculator.p.rapidapi.com'
+// 	}
+// };
+// async function fetchData(){
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// }
+// fetchData()
+
+// let body = document.getElementById("body")
+// let fnammeJs = document.getElementById("fname")
+// let snameJs = document.getElementById("sname")
+// let percentageJs = document.getElementById("percentage")
+// let resultJs = document.getElementById("result")
+
+
+
+// console.log(JSON.parse(fetchData()))
