04024c5716f7576abcf9afe663e36e199fb21d5a
liorthr <liortahar116@gmail.com>
2024-02-04 11:24:46 +0200

    mini project done

--- week 10/day 6/mini project/miniProjectD6W10.js
+++ week 10/day 6/mini project/miniProjectD6W10.js
@@ -1,27 +1,34 @@
-let squareJS = document.getElementById("square")
-let displayName = document.getElementById("display-name")
-let displayHeight = document.getElementById("display-height")
-let displayGender = document.getElementById("display-gender")
-let displayBirthYear = document.getElementById("display-birth-year")
-let displayHomeworld = document.getElementById("display-home-world")
-let buttonJS = document.getElementById("centered-button")
-const peopleNumber = 5
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-fetch(`https://www.swapi.tech/api/people/${peopleNumber}`)
+let buttonJS = document.getElementById("centered-button");
+buttonJS.addEventListener("click", displayRandomPersonStarWars);
+
+async function displayRandomPersonStarWars() {
+    const peopleNumber = getRandomNumber(1, 83);
+    
+    fetch(`https://www.swapi.tech/api/people/${peopleNumber}`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        const nameJS = data.result.properties.name
-        displayName.textContent = `${nameJS}`
-        const heightJS = data.result.properties.height
-        displayHeight.textContent = `HEIGHT: ${heightJS}`
-        const genderJS = data.result.properties.gender
-        displayGender.textContent = `GENDER: ${genderJS}`
-        const birthYearJS = data.result.properties.birth_year
-        displayBirthYear.textContent = `BIRTH YEAR: ${birthYearJS}`
-        const homeWorldJS = data.result.properties.homeworld
-        displayHomeworld.textContent = `HOME WORLD: ${homeWorldJS}`
+        const nameJS = data.result.properties.name;
+        let displayName = document.getElementById("display-name");
+        displayName.textContent = `${nameJS}`;
+        const heightJS = data.result.properties.height;
+        let displayHeight = document.getElementById("display-height");
+        displayHeight.textContent = `HEIGHT: ${heightJS}`;
+        const genderJS = data.result.properties.gender;
+        let displayGender = document.getElementById("display-gender");
+        displayGender.textContent = `GENDER: ${genderJS}`;
+        const birthYearJS = data.result.properties.birth_year;
+        let displayBirthYear = document.getElementById("display-birth-year");
+        displayBirthYear.textContent = `BIRTH YEAR: ${birthYearJS}`;
+        const homeWorldJS = data.result.properties.homeworld;
+        let displayHomeworld = document.getElementById("display-home-world");
+        displayHomeworld.textContent = `HOME WORLD: ${homeWorldJS}`;
     })
     .catch(error => {
         console.error('Error fetching data:', error);
     });
+}
