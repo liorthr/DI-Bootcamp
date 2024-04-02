d573ae01cb02028938eb1867dc0135b5b8032236
liorthr <liortahar116@gmail.com>
2024-04-02 12:11:49 +0300

    review python

---
+++ week-19/day-2/exercises-xp/ex_xp.py
@@ -0,0 +1,65 @@
+##EX 1
+
+# print(4*'Hello World\n')
+
+##EX 2
+
+# calcul = (99**3)*8
+# print(calcul)
+
+##EX 3
+# print(5 < 3)
+#False
+# print(3 == 3)
+# True
+# print(3 == "3")
+# False
+# print("3" > 3)
+# Error 
+# print("Hello" == "hello")
+# False
+
+##EX 4
+
+# computer_brand = 'Macintosh'
+# sentence = (f'I have a {computer_brand} computer')
+# print(sentence)
+
+##EX 5
+
+# name = 'Lior'
+# age = 20
+# shoe_size = 42
+# info = f'Hey my name is {name} i have {age} years old ans my shoe size is {shoe_size}'
+# print(info)
+
+##EX 6
+
+# a = 10
+# b = 15
+# if (a>b ):
+#     print('Hello World')
+
+##EX 7
+
+# number_user = input('Enter your number:  ')
+# if(int(number_user) % 2 == 0):
+#     print('You choose a even number ')
+# else:
+#     print('You choose a odd number')
+
+##EX 8
+
+# name_user = input('What is your name bro 🧏‍♂️   ')
+# if(name_user == 'Lior'):
+#     print('Hey same name bro')
+# else:
+#     print('Your son maybe')
+
+##EX 9
+
+height_user = input('What is your size:   ')
+if(int(height_user)>145):
+    print('You can ride')
+else:
+    print('not yet')
