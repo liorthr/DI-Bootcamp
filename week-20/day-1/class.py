b23dfb64a7b1fe90f864a913734aa7a212e7a1a0
liorthr <liortahar116@gmail.com>
2024-04-07 21:08:16 +0300

    class review py

---
+++ week-20/day-1/class.py
@@ -0,0 +1,43 @@
+# def say_hello():
+#     username = input('Hey what your name:  ')
+#     print(f'Hello {username}')
+    
+# say_hello()
+
+# def say_hello(username, language):
+#     if language == "EN":
+#         print("Hello "+username)
+#     elif language == "FR":
+#         print("Bonjour "+username)
+#     else:
+#         print("This language is not supported: " + language)
+
+# say_hello(username="Albert", language="FR")
+
+# def divide_by_three(number):
+#   return number / 3
+
+# first_number = 12
+# first_number_computed = divide_by_three(first_number)
+# print(first_number_computed)
+
+# def calculations(a,b):
+#     return a+b
+# res = calculations(2, 24)
+# print(res)
+
+# def merge_and_sort_list(list1, list2):
+#     merge_list= list1+list2
+#     unique_sorted_list=sorted(set(merge_list))
+#     return unique_sorted_list
+
+# list4= ['g','z','y','a']
+# list3= ['a', 'b', 'c']
+# print(merge_and_sort_list(list1=list4, list2=list3))
+
+# def group_anagrams(words):
+#     new_words = sorted(set(words))
+#     for i in new_words:
+#         if 
+#     print(new_words)
+# group_anagrams(['ballon', 'pirate', "abeille", 'test'])
