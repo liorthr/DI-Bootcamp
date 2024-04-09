aa51714b86a5ec115dfdf289b7516d42c51ffdaf
liorthr <liortahar116@gmail.com>
2024-04-09 17:30:34 +0300

    ex xp done

---
+++ week-20/day-2/ep/ex_xp.py
@@ -0,0 +1,44 @@
+##EX 1
+# def display_message(topic):
+#     print(f'We learn {topic} at DI')
+
+# display_message('Python')
+
+##EX 2
+# def favorite_book(title):
+#     print(f"One of my favorite books is {title}")
+# favorite_book('Alice in the world')
+
+##EX 3
+# def describe_city(city, country):
+#     print(f'{city} is in {country}')
+# describe_city(city='Paris', country='France')
+
+##EX 4
+# import random
+# def number(a):
+#     1<a<100
+#     b = random.randint(1,100)
+#     if(a==b):
+#         print('Success')
+#     else:
+#         print('Fail')
+# number(24)
+
+##EX 5
+# def make_shirt(text, size='Large'):
+#     print(f"The size of the shirt is <{size}> and the text is <{text}>")
+# make_shirt('test 2')
+# make_shirt('Test 3', size='Medium')
+
+##EX 6
+# magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+# def show_magicians(list):
+#     for name in list:
+#         print(name)
+# show_magicians(magician_names)
+
+# def show_magicians(list2):
+#     for name in list2:
+#         print(f'{name} the gerat')
+# show_magicians(magician_names)
