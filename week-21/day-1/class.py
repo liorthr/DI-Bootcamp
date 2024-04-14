42b9e088e81d8eedf64f8c676f797fb385d1fe05
liorthr <liortahar116@gmail.com>
2024-04-14 20:25:22 +0300

    class in py

---
+++ week-21/day-1/class.py
@@ -0,0 +1,32 @@
+# class DogData():
+#     def __init__(self, name_of_the_dog, amount_of_legs):
+#         self.name = name_of_the_dog
+#         self.amount_of_legs = amount_of_legs
+#     def __str__(self):
+#         return (f'name: {self.name} amount: {self.amount_of_legs}')
+#     def names(self, other_dog):
+#         print()
+        
+
+# class BankAccount():
+#     def __init__(self, owner, balance ):
+#         self.owner = owner
+#         self.balance = balance
+#         print(f'The owner is {owner} and have {balance}$')
+#     def deposit(self, amount):
+#         self.balance += amount 
+#         print(f'You add {amount}$ new balan,ce {self.balance}')
+#     def withdraw(self, amount): 
+#         self.balance -= amount
+#         print(f'You withdraw {amount}$')
+#     def get_balance(self):
+#         print(f'The finale balance is {self.balance}')
+
+# user_01 = BankAccount('David', 1000)  
+# user_01.deposit(1000)
+# user_01.withdraw(500)
+# user_01.get_balance()
+
+
+class Book():
+    
