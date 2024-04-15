3e570e3d5180af1f9d32246f938f6eea32c059d4
liorthr <liortahar116@gmail.com>
2024-04-15 16:59:38 +0300

    dc done

---
+++ week-21/day-2/dc/dc.py
@@ -0,0 +1,12 @@
+# class Farm():
+#     def __init__(self, name):
+#         self.name = name
+#         self.list_of_animals = []
+#     def add_animals(self, name):
+#         name =''
+#         number = 0
+#         self.list_of_animals.append(name)
+#     def get_info(self):
+#         return self.list_of_animals
+    
+        
