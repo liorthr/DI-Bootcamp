1f38175c3e5767eced04119d4a32e2ed73560352
liorthr <liortahar116@gmail.com>
2024-05-13 18:24:36 +0300

    create a function to find the most longest word and the most smallest word

--- week-23/leetcode/leetcode.py
+++ week-23/leetcode/leetcode.py
@@ -16,10 +16,29 @@
 # test = Solution()
 # print(test.mergeAlternately(word1= 'abc', word2 = 'pqroh'))
 
-def reversed(string):
-    s = ''
-    for c in string:
-        s = c + s
-    print(s)
+# def reversed(string):
+#     s = ''
+#     for c in string:
+#         s = c + s
+#     print(s)
 
-reversed('Bonjour')
+# reversed('Bonjour')
+
+sentence_01 = 'Aujourdhui on va aller à la plage avec mes amis'
+
+# def findBigWrod(sentence):
+#     biggestword = ''
+#     for word in sentence:
+#         if word > biggestword:
+#             biggestword = word
+#     print(biggestword)
+
+# findBigWrod(sentence_01)
+
+def findBigWrod(sentence):
+    sentence_split = sentence.split()
+    max_word = max(sentence_split)
+    min_word = min(sentence_split)
+    print('max word: ', max_word)
+    print('min word: ', min_word)
+findBigWrod(sentence_01)
