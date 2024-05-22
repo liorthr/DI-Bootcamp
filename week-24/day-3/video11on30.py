03da743de1064b7379d72f4a02669ee789790352
liorthr <liortahar116@gmail.com>
2024-05-22 12:14:30 +0300

    numpy array of image with scipy

--- week-24/day-3/video11on30.py
+++ week-24/day-3/video11on30.py
@@ -6,7 +6,7 @@
 #exemple
 import numpy as np 
 
-A = np.array([[1,2,3], [4,5,6], [7,8,9]])
+# A = np.array([[1,2,3], [4,5,6], [7,8,9]])
 # print(A)
 #accéder au premier éléments
 # print(A[0,0]) #en gros A[row, column]
@@ -28,7 +28,30 @@ A = np.array([[1,2,3], [4,5,6], [7,8,9]])
 # print(B)
 
 ##execice hard
-C = np.zeros((5,5))
-print(C)
-C[0:5:2 , 0:5:2] = 1
-print(C)
+# C = np.zeros((5,5))
+# print(C)
+# C[0:5:2 , 0:5:2] = 1
+# print(C)
+
+
+"""
+BOOLEAN INDEXING
+"""
+
+# A = np.random.randint(0,10, [5,5])
+# print(A)
+# A[A<5] = 10
+# print(A)
+
+# IMAGE = np.random.randint(0, 255, [1024, 720])
+# print(IMAGE)
+
+##EXERCICE IMAGE
+from scipy import misc
+import matplotlib.pyplot as plt 
+face = misc.face(gray = True)
+plt.imshow(face, cmap = plt.cm.gray)
+plt.show()
+print(f'Type is: {type(face)}')
+print(f'Ndim is: {face.shape}')
+print(f'Shape is: {face.ndim}')
