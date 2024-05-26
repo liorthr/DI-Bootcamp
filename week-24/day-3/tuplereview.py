9215cdd9930ac283c1b2fe75254a2efe78b0e535
liorthr <liortahar116@gmail.com>
2024-05-26 13:20:15 +0300

    numpy statistics advanced

---
+++ week-24/day-3/tuplereview.py
@@ -0,0 +1,9 @@
+liste = [1,24,'Jackie',67,98,34,67,'Albert']
+
+# for i in enumerate(liste):
+#     print(i)
+
+##tuple toujours une virgule au min
+
+test_tuple = (45,)
+print(type(test_tuple))
--- week-24/day-3/video12on30.py
+++ week-24/day-3/video12on30.py
@@ -12,7 +12,41 @@ Rappel: .axis = 0 c'est verticale ca va vers le bas en gros l'axe y
 """
 
 
-B = np.array([1, 3, -2, 5])
-print(B)  
-B.sort()
-print(B)  
+B = np.cos([1, 3, -2, 5])
+# print(B)  
+# B.sort()
+# print(B)  
+
+"""
+Numpy fournit des functions mais a une limite mathématiques pour avoir un plus large 
+choix on utilise https://docs.scipy.org/doc/numpy-1.13.0/reference/routines.math.html
+Il suffit de mettre np.function_name(array)
+"""
+
+# print(np.log(B))
+
+
+"""
+STATISTIQUES
+"""
+Y = np.random.randint(0, 10 , [10,10])
+# print(Y)
+# print(Y.mean())
+# print(Y.var()) 
+# print(Y.std())
+
+##CHECKER LE NB DE REPETTIONS
+values , counts = np.unique(Y, return_counts=True)
+# print(values[counts.argsort()])
+
+"""
+Quand on recoit bcp de data on doit filter nettoyer 
+
+"""
+Z = np.random.randn(5,5)
+Z[0,2] = np.nan
+Z[4,3] = np.nan
+print(Z.mean())
+print(np.nanmean(Z))
+print(f' sum of nan {np.isnan(Z).sum()}')
+print(f' size of nan {np.isnan(Z).sum()/Z.size}')
