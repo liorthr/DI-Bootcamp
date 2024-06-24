ca71e80128445198592a6d1873cb0a9f555e3a97
liorthr <liortahar116@gmail.com>
2024-06-24 20:55:08 +0300

    leetcode

---
+++ week-29/day-1/leetcode.py
@@ -0,0 +1,13 @@
+# liste_01 = [1, 5, 6, 7, 9, 10, 11]
+# liste_02 = [2, 3, 5, 7, 8, 10, 12]
+
+liste_01 = [1, 5, 6, 7, 9, 10, 11]
+liste_02 = [2, 3, 5, 7, 8, 10, 12]
+resultat = []
+for i in liste_01:
+    if i in liste_02:
+        resultat.append(i)
+
+# resultat = set(liste_01) & set(liste_02)
+print(resultat)
+
