08e5ad0a4fa8a374b9ce9880bd6c38dc44b6cea7
liorthr <liortahar116@gmail.com>
2024-07-01 12:13:08 +0300

    number leetcode

---
+++ week-30/day-2/number.py
@@ -0,0 +1,18 @@
+nombre = 209812
+sum = 0
+for number in str(nombre):
+    sum += int(number)
+print(sum)
+
+# i = 1
+# for index, number in enumerate(str(nombre)):
+#     # print(index, number)
+#     print((number[index]))
+    
+
+# i = 0
+# for i in str(nombre):
+#     sum = str(nombre)[i]
+#     i += 1
+#     print(sum)
+
