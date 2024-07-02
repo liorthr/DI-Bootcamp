593d263d684a9f64ac97dd15f3be077515b9f2cc
liorthr <liortahar116@gmail.com>
2024-07-02 11:28:29 +0300

    slice leetcode

---
+++ week-30/day-3/silce.py
@@ -0,0 +1,15 @@
+# a = ("a", "b", "c", "d", "e", "f", "g", "h")
+# x = slice(2)
+# print(a[x])
+
+
+site_web = "Docstring"
+langage = "Python"
+prenom = "Patrick"
+
+le_doc = site_web[0:3]
+print(le_doc)
+le_python = langage[2:6]
+print(le_python)
+lettre = prenom[0]
+print(lettre)
